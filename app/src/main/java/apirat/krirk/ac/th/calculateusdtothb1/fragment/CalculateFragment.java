49e7c0ad4f13fe7c291a3c2e54e7442a97e796c7
Apirat <apirat.on2018@gmail.com>
2018-02-27 11:04:15 +0700

    MainFragment Back Controller

--- app/src/main/java/apirat/krirk/ac/th/calculateusdtothb1/fragment/CalculateFragment.java
+++ app/src/main/java/apirat/krirk/ac/th/calculateusdtothb1/fragment/CalculateFragment.java
@@ -4,10 +4,12 @@ package apirat.krirk.ac.th.calculateusdtothb1.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import apirat.krirk.ac.th.calculateusdtothb1.MainActivity;
 import apirat.krirk.ac.th.calculateusdtothb1.R;
 
 /**
@@ -15,6 +17,31 @@ import apirat.krirk.ac.th.calculateusdtothb1.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolber
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+//        Exchange Controller
+
+
+
+    }// Main Method
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
