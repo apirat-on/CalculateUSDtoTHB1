1e5f040ee4c4cafb603c74eee343615d725e144f
Apirat <apirat.on2018@gmail.com>
2018-02-27 10:36:41 +0700

    MainFragment

--- app/src/main/java/apirat/krirk/ac/th/calculateusdtothb1/MainActivity.java
+++ app/src/main/java/apirat/krirk/ac/th/calculateusdtothb1/MainActivity.java
@@ -3,11 +3,19 @@ package apirat.krirk.ac.th.calculateusdtothb1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import apirat.krirk.ac.th.calculateusdtothb1.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
     }
 }
---
+++ app/src/main/java/apirat/krirk/ac/th/calculateusdtothb1/fragment/MainFragment.java
@@ -0,0 +1,46 @@
+package apirat.krirk.ac.th.calculateusdtothb1.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import apirat.krirk.ac.th.calculateusdtothb1.R;
+
+/**
+ * Created by playn on 2/27/2018.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Calculate Controller
+        Button button = getView().findViewById(R.id.btnCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_main, container ,false);
+return view;
+    }
+}
